f81fe7fb65996263293030eea4122efc7f4469ca
deanyarongit <deany@perfectomobile.com>
2024-01-22 17:41:55 +0200

    update axios version to 0.21.2

--- src/index.js
+++ src/index.js
@@ -1,105 +1,106 @@
 /* global Cypress, cy */
 const axios = require('axios');
 const {
-  REPORTING_TEST_STATUS,
-  LAB_EXECUTION_REPORT_URL,
-  MOCHA_STATUS,
-  REPORTING_COMMAND_STATUS
+    REPORTING_TEST_STATUS,
+    LAB_EXECUTION_REPORT_URL,
+    MOCHA_STATUS,
+    REPORTING_COMMAND_STATUS
 } = require('./consts');
 const commandHandler = require('./command-handler');
 
-const ignoreReporterErrors = () => {/* do nothing this is not relevant to the execution */};
+let testStartTime = 0;
 
-const getFiledRecursively = (field, data, delimiter = ' - ') => {
-  if (!data.parent) return data[field];
-  let parentsValue = getFiledRecursively(field, data.parent, delimiter);
-
-  return (parentsValue ? parentsValue + delimiter : '') + data[field];
+const ignoreReporterErrors = () => {/* do nothing this is not relevant to the execution */
 };
 
-const getSpecFile = () =>  Cypress.spec && Cypress.spec.relative;
+const getFiledRecursively = (field, data, delimiter = ' - ') => {
+    if (!data.parent) return data[field];
+    let parentsValue = getFiledRecursively(field, data.parent, delimiter);
+
+    return (parentsValue ? parentsValue + delimiter : '') + data[field];
+};
+
+const getSpecFile = () => Cypress.spec && Cypress.spec.relative;
 
 const getCustomFields = () => {
-  const customFields = [];
+    const customFields = [];
 
-  let specFile = getSpecFile();
+    let specFile = getSpecFile();
 
-  if (specFile) {
-    customFields.push({name: 'SpecFile', value: specFile})
-  }
+    if (specFile) {
+        customFields.push({name: 'SpecFile', value: specFile})
+    }
 
-  if (Cypress.version) {
-    customFields.push({name: 'CypressVersion', value: Cypress.version})
-  }
+    if (Cypress.version) {
+        customFields.push({name: 'CypressVersion', value: Cypress.version})
+    }
 
-  return customFields;
+    return customFields;
 };
 
 const isFailed = (test) => {
-  return test.state === MOCHA_STATUS.FAILED;
+    return test.state === MOCHA_STATUS.FAILED;
 };
 
-Cypress.on('script:error', function(err){
-  return axios.post(
-    LAB_EXECUTION_REPORT_URL + '/execution-data/',
-    {
-      ...err && err.error && {failedMsg: err.error}
-    }
-  );
+Cypress.on('script:error', function (err) {
+    return axios.post(
+        LAB_EXECUTION_REPORT_URL + '/execution-data/',
+        {
+            ...err && err.error && {failedMsg: err.error}
+        }
+    );
 });
 
-Cypress.on('test:before:run', async (_test, runner) =>{
-  const testStartTime = new Date().getTime();
-  let failedCommand;
-
-  // TODO: (Elhay) second test not reported??
-  // TODO: (Elhay) commands not reported??
-  cy.on('test:after:run', async (test) => {
+Cypress.on('test:after:run', (test, runner) => {
     let isTestFailed = isFailed(test);
     const status = isTestFailed ? REPORTING_TEST_STATUS.FAILED : REPORTING_TEST_STATUS.PASSED;
     const message = isTestFailed ? test.err.stack : '';
 
     const testEndTime = new Date().getTime();
     return axios.post(
-      LAB_EXECUTION_REPORT_URL + '/test-end/',
-      {
-        status,
-        message,
+        LAB_EXECUTION_REPORT_URL + '/test-end/',
+        {
+            status,
+            message,
+            name: getFiledRecursively('title', runner),
+            specFile: getSpecFile(),
+            endTime: testEndTime,
+            duration: testEndTime - testStartTime
+        }
+    ).catch(ignoreReporterErrors);
+});
+
+Cypress.on('test:before:run', async (_test, runner) => {
+    testStartTime = new Date().getTime();
+    let failedCommand;
+
+    cy.on('fail', (error) => {
+        if (failedCommand) {
+            axios.post(
+                LAB_EXECUTION_REPORT_URL + '/command/',
+                commandHandler.getCommandParams(failedCommand, REPORTING_COMMAND_STATUS.FAILURE)
+            ).catch(ignoreReporterErrors);
+        }
+        throw error;
+    });
+
+    cy.on('command:start', (command) => {
+        command.startTime = new Date().getTime();
+        failedCommand = command;
+    });
+
+    cy.on('command:end', (command) => {
+        axios.post(
+            LAB_EXECUTION_REPORT_URL + '/command/',
+            commandHandler.getCommandParams(command)
+        ).catch(ignoreReporterErrors);
+        // TODO: (Elhay) try  to report from here about failed command if  we have the status
+    });
+
+    return axios.post(LAB_EXECUTION_REPORT_URL + '/test-start', {
         name: getFiledRecursively('title', runner),
+        startTime: testStartTime,
         specFile: getSpecFile(),
-        endTime: testEndTime,
-        duration: testEndTime - testStartTime
-      }
-    ).catch(ignoreReporterErrors);
-  });
-
-  cy.on('fail', (error) => {
-    if (failedCommand) {
-      axios.post(
-        LAB_EXECUTION_REPORT_URL + '/command/',
-        commandHandler.getCommandParams(failedCommand, REPORTING_COMMAND_STATUS.FAILURE)
-      ).catch(ignoreReporterErrors);
-    }
-    throw error;
-  });
-
-  cy.on('command:start', (command) => {
-    command.startTime = new Date().getTime();
-    failedCommand = command;
-  });
-
-  cy.on('command:end', (command) => {
-    axios.post(
-      LAB_EXECUTION_REPORT_URL + '/command/',
-      commandHandler.getCommandParams(command)
-    ).catch(ignoreReporterErrors);
-    // TODO: (Elhay) try  to report from here about failed command if  we have the status
-  });
-
-  return axios.post(LAB_EXECUTION_REPORT_URL + '/test-start', {
-    name: getFiledRecursively('title', runner),
-    startTime: testStartTime,
-    specFile: getSpecFile(),
-    context: { customFields: [...getCustomFields()] }
-  }).catch(ignoreReporterErrors);
+        context: {customFields: [...getCustomFields()]}
+    }).catch(ignoreReporterErrors);
 });
